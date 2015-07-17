fad15af181c4debf304782c386fc245457377eaa
Matthew Beaudouin <MatthewBeaudouin@Matthews-MacBook-Pro.local>
2015-07-17 11:54:03 +0200

    Initial Commit

---
+++ Board Game/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Board Game
+//
+//  Created by Matthew Beaudouin on 7/17/15.
+//  Copyright (c) 2015 Matthew Beaudouin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
